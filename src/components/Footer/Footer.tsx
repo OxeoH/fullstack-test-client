8f8421307d9b9fe0bdd25b4be8f45e017e612b0a
OxeoH <egor33429@gmail.com>
2023-08-03 20:52:46 +0300

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+import WhatsApp from '../../../public/assets/icons/whatsapp.svg'
+import Viber from '../../../public/assets/icons/viber.svg'
+import Telegram from '../../../public/assets/icons/telegram.svg'
+import { InfoBlock } from '../InfoBlock/InfoBlock'
 
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.footer}>
+      <InfoBlock>
+        <h2 className={styles.title}>contacts</h2>
+        <div className={styles.contacts}>
+          <div className={styles.contact}>
+            <Telegram />
+            <h4 className={styles.social}>telegram</h4>
+          </div>
+          <div className={styles.contact}>
+            <WhatsApp />
+            <h4 className={styles.social}>whatsapp</h4>
+          </div>
+          <div className={styles.contact}>
+            <Viber />
+            <h4 className={styles.social}>viber</h4>
+          </div>
+        </div>
+      </InfoBlock>
+    </div>
+  )
 }
